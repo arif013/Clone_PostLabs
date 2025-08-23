8df42ae069e172293f3f99226adcb295ec11c46f
arif013 <mdarif11099@gmail.com>
2025-08-23 14:01:45 +0530

    parallax going on

--- src/App.jsx
+++ src/App.jsx
@@ -6,24 +6,29 @@ import WordAnimation from './components/WordAnimation';
 import { useGSAP } from '@gsap/react';
 import Video from './components/Video';
 import Building from './components/Building';
+import ParallaxVideo from './components/ParallaxVideo';
 
 gsap.registerPlugin(ScrollTrigger, SplitText, ScrollSmoother);
 
 
 const App = () => {
-    useLayoutEffect(()=>{
+    useLayoutEffect(() => {
         ScrollSmoother.create({
-            smooth:1,
+            smooth: 1,
         })
-    },[])
+    }, [])
     return (
         <div id='smooth-wrapper'>
             <div id='smooth-content'>
                 <main id='landing-page'>
                     <Hero />
                     <WordAnimation />
-                    <Video/>
-                    <Building/>
+                    <Video />
+                    <Building />
+                    <ParallaxVideo />
+                    <div className="h-[200vh] bg-gray-900 text-white flex items-center justify-center text-4xl">
+                        Next Page Content 🚀
+                    </div>
                 </main>
             </div>
         </div>
--- src/components/Building.jsx
+++ src/components/Building.jsx
@@ -1,40 +1,59 @@
+import { useGSAP } from '@gsap/react'
+import { SplitText } from 'gsap/all'
+import gsap from 'gsap'
 import React from 'react'
 
-const data = [
-    {
-        item0: "",
-        item: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit alias nulla veritatis expedita recusandae enim blanditiis animi sequi, repellendus repellat!",
-        item1: "",
-        class: "col-span-2"
-    },
-    {
-        item0: "",
-        item1: "",
-        item: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit alias nulla veritatis expedita recusandae enim blanditiis animi sequi, repellendus repellat!",
-        class: "col-span-2"
-    },
-    {
-        item0: "",
-        item: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit alias nulla veritatis expedita recusandae enim blanditiis animi sequi, repellendus repellat!",
-        item1: "",
-        class: "col-span-2"
-    }
-]
 
 const Building = () => {
+    useGSAP(()=>{
+        gsap.utils.toArray(".animate-text").forEach(el=>{
+            const wordSplit = new SplitText(el, {type: "words, lines"})
+            
+            gsap.from(wordSplit.words, {
+                opacity: 0,
+                duration: 1.5,
+                ease: "expo.in",
+                stagger: 0.06,
+                scrollTrigger: {
+                    trigger: el,
+                    start: "top 80%",
+                    end: "bottom top",
+                    // scrub: true
+                    toggleActions: "play none none none"
+                }
+            })
+            
+        //     gsap.timeline({
+        //         scrollTrigger: {
+        //         trigger: el,
+        //         start: "top 50%",
+        //         end: "bottom top",
+        //         scrub: true
+        //     }
+        // })
+    })
+    }, [])
     return (
         <section>
-            <div className='text-[144px] px-[40px] py-[100px] leading-none'>What We're <br /> Building</div>
-            {data.map((dataItem, index) => (
+            <div  className='animate-text text-[144px] px-[40px] py-[100px] leading-none '>What We're <br /> Building</div>
                 <div className='grid grid-cols-4 items-center py-[220px]'>
-                    <>
-                        {/* <div></div> */}
-
-                        <p className='col-span-2 text-[36px]'>{dataItem.item}</p>
-                        {/* <div></div> */}
-                    </>
+                    <div></div>
+                    
+                        <p  className={`animate-text col-span-2 text-[36px]`}> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Harum, magni laboriosam? </p>
+                        <div></div>
                 </div>
-            ))}
+                <div className='grid grid-cols-4 items-center py-[220px]'>
+                    <div></div>
+                    <div></div>
+                        <p  className={` animate-text col-span-2 text-[36px]`}> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Harum, magni laboriosam? </p>
+                        
+                </div>
+                <div className='grid grid-cols-4 items-center py-[220px]'>
+                    <div></div>
+                    
+                        <p  className={` animate-text col-span-2 text-[36px]`}> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Harum, magni laboriosam? </p>
+                        <div></div>
+                </div>
             <div>
 
             </div>
